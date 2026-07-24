4fa74175d228b082265ef3cc2ed6bdaf0105bb23
Wudong Liu <wudong.liu@gmail.com>
2026-07-24 15:23:24 +0100

    Load native mobile interaction styles

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import './design-tokens.css';
+import './native-mobile.css';
 import { AppRouter } from './AppRouter';
 
 const rememberedTheme = window.localStorage.getItem('TTPlayers-Theme');
