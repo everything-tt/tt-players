aed0f976809ba1fbea9f19d571ddc33dc1906cf7
Wudong Liu <wudong.liu@gmail.com>
2026-07-31 07:55:04 +0100

    add read model database types

---
+++ packages/db/src/read-model-types.ts
@@ -0,0 +1,38 @@
+import type { ColumnType, Insertable, Selectable, Updateable } from 'kysely';
+
+export interface DataVersionsTable {
+    key: string;
+    version: ColumnType<string, number | string | undefined, number | string>;
+    updated_at: ColumnType<Date, Date | undefined, Date>;
+}
+
+export type DataVersion = Selectable<DataVersionsTable>;
+export type NewDataVersion = Insertable<DataVersionsTable>;
+export type DataVersionUpdate = Updateable<DataVersionsTable>;
+
+export interface SourceQualitySnapshotsTable {
+    key: string;
+    content: unknown;
+    generated_at: ColumnType<Date, Date | string, Date | string>;
+    updated_at: ColumnType<Date, Date | undefined, Date>;
+}
+
+export type SourceQualitySnapshot = Selectable<SourceQualitySnapshotsTable>;
+export type NewSourceQualitySnapshot = Insertable<SourceQualitySnapshotsTable>;
+export type SourceQualitySnapshotUpdate = Updateable<SourceQualitySnapshotsTable>;
+
+export interface PlayerActiveLeaguesTable {
+    player_id: string;
+    league_id: string;
+    updated_at: ColumnType<Date, Date | undefined, Date>;
+}
+
+export type PlayerActiveLeague = Selectable<PlayerActiveLeaguesTable>;
+export type NewPlayerActiveLeague = Insertable<PlayerActiveLeaguesTable>;
+export type PlayerActiveLeagueUpdate = Updateable<PlayerActiveLeaguesTable>;
+
+export interface ReadModelDatabase {
+    data_versions: DataVersionsTable;
+    source_quality_snapshots: SourceQualitySnapshotsTable;
+    player_active_leagues: PlayerActiveLeaguesTable;
+}
