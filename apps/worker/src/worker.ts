b4df88e75a831c1a0cf035d7bdb5099ded9d75da
Wudong Liu <wudong.liu@gmail.com>
2026-07-31 10:46:10 +0100

    Run derived data after ingestion completes

--- apps/worker/src/worker.ts
+++ apps/worker/src/worker.ts
@@ -57,9 +57,7 @@ const CRONTAB = `
  30 2 * * * scrapeSport80EventsTask ?fill=1d
  0 3 * * * scrapeSport80RankingsDiscoveryTask ?fill=1d
  30 3 * * * purgeExpiredCacheEntries ?fill=1d
- 45 3 * * * reconcilePlayersTask ?fill=1d
- 0 4 * * * calculateRatingsTask ?fill=1d
- 30 4 * * * refreshApiReadModelsTask ?fill=1d
+ 45 3 * * * completeDailyPipelineTask ?fill=1d
  `;
 
 export async function startWorker(): Promise<void> {
